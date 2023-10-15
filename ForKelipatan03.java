4bb86e90a8766d040729f54a5b48331ff5b69118
annisaeka123 <annisaekapuspitaaa@gmail.com>
2023-10-16 01:52:11 +0700

    kelipatan1

--- ForKelipatan03.java
+++ ForKelipatan03.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kelipatan, total=0, counter=0;
+        double rata_rata = 0.0;
 
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
@@ -18,8 +19,12 @@ import java.util.Scanner;
             }
         }
 
+        if (counter > 0) {
+            rata_rata = (double) total / counter;
+        }
+
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
-
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rata_rata);
     }
 }
