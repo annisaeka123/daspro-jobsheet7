b905d4f027678a09ae24cbf47ea6ddc8c280d27c
annisaeka123 <annisaekapuspitaaa@gmail.com>
2023-10-18 13:57:37 +0700

    jatah cuti

---
+++ DoWhileCuti03.java
@@ -0,0 +1,34 @@
+/**
+ * DoWhileCuti03
+ */
+import java.util.Scanner;
+ public class DoWhileCuti03 {
+
+    public static void main(String[] args) {
+        java.util.Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah Cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah Hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                   
+                }
+            }
+        
+        }while (jatahCuti > 0);
+    } 
+}
